8ccd0eed0f0b696e02716bcabde277c5ac7f72d1
thothapps <thothapp.ti@gmail.com>
2026-01-08 14:46:06 -0300

    teste_vw

--- public/mongo/routes/registros.js
+++ public/mongo/routes/registros.js
@@ -49,7 +49,7 @@ module.exports = (app, dbConnection) => {
         } = req.body;
 
         console.log('/_bd/registro::' + tokem + '::' + tag)
-        console.log('/_bd/registro::' + req.body)
+        console.log('/_bd/registro::' + JSON.stringify(req.body))
 
         // Verificar Localização
         let retorno;
