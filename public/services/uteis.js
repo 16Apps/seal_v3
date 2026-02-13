413b6c1846650918e0a84863e80fa1430aee43ba
thothapps <thothapp.ti@gmail.com>
2026-02-13 16:06:17 -0300

    deploy_start

--- public/services/uteis.js
+++ public/services/uteis.js
@@ -3,8 +3,8 @@ app.service('uteisService', ['$rootScope', '$http', function ($rootScope, $http)
   moment.locale('pt-br');
 
   const service = this;
-  // service.ipAPI = 'http://localhost:5000';
-  service.ipAPI = 'https://sealairtracking-3d3268c3e73f.herokuapp.com'
+  service.ipAPI = 'http://localhost:3000';
+  // service.ipAPI = 'https://sealairtracking-3d3268c3e73f.herokuapp.com'
   // service.ipAPI = 'https://connectiot-app.azurewebsites.net'
 
   // api
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var http = require('http');
 const socketio = require('socket.io');
 
 var app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 server = http.createServer(app);
 
